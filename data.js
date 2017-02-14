96b3d84a9b8b5604a5c114505aadece29ae5b3fc
HavahLT <tourbillon.laurianne@gmail.com>
2017-02-14 11:40:03 +0100

    ajout des fichiers du pokedex

---
+++ data.js
@@ -0,0 +1,83 @@
+var pkmnDonnees = [
+  {
+    numero:"001",
+    nom:"Bulbizarre",
+    img:"http://pokeunlock.com/wp-content/uploads/2015/01/bulbasaur.gif",
+    shiny:"http://pokeunlock.com/wp-content/uploads/2015/01/bulbasaur-2.gif",
+    type1:"plante",
+    type2:"poison",
+    description:"La graine sur le dos de Bulbizarre est une réserve de nutriments. Il la fait doucement pousser en absorbant les rayons du soleil, et emmagasine de l'énergie dedans en vue de son évolution. S'il dépense trop souvent son énergie, il n'évoluera pas car l'ouverture de son bulbe dépend de ses réserves de nutriments."
+  },
+  {
+    numero:"002",
+    nom:"Herbizarre",
+    img:"http://pokeunlock.com/wp-content/uploads/2015/01/ivysaur.gif",
+    shiny:"http://pokeunlock.com/wp-content/uploads/2015/01/ivysaur-2.gif",
+    type1:"plante",
+    type2:"poison",
+    description:"Herbizarre est un Pokémon quadrupède, similaire à un dinosaure. Il a un corps bleu-vert, avec des taches plus foncées. Deux dents pointues proviennent de sa mâchoire supérieure, et ses yeux se sont rétrécis et sont devenus pourpres."
+  },
+  {
+    numero:"003",
+    nom:"Florizarre",
+    img:"http://pokeunlock.com/wp-content/uploads/2015/01/venusaur.gif",
+    shiny:"http://pokeunlock.com/wp-content/uploads/2015/01/venusaur-2.gif",
+    type1:"plante",
+    type2:"poison",
+    description:"Florizarre est un imposant quadrupède à la peau verte ayant l'aspect d'un batracien, il est beaucoup plus grand et plus lourd que Bulbizarre et Herbizarre. Il porte une grande fleur sur son dos et quatre grandes feuilles, l'intérieur de ses oreilles s'est coloré de rouge et sa peau au niveau de ses pattes est désormais recouverte d'excroissances faisant penser aux verrues d'un crapaud."
+  },
+  {
+    numero:"004",
+    nom:"Salamèche",
+    img:"http://pokeunlock.com/wp-content/uploads/2015/01/charmander.gif",
+    shiny:"http://pokeunlock.com/wp-content/uploads/2015/01/charmander-2.gif",
+    type1:"feu",
+    type2:"null",
+    description:"Florizarre est un imposant quadrupède à la peau verte ayant l'aspect d'un batracien, il est beaucoup plus grand et plus lourd que Bulbizarre et Herbizarre. Il porte une grande fleur sur son dos et quatre grandes feuilles, l'intérieur de ses oreilles s'est coloré de rouge et sa peau au niveau de ses pattes est désormais recouverte d'excroissances faisant penser aux verrues d'un crapaud."
+  },
+  {
+    numero:"005",
+    nom:"Reptincel",
+    img:"http://pokeunlock.com/wp-content/uploads/2015/01/charmeleon.gif",
+    shiny:"http://pokeunlock.com/wp-content/uploads/2015/01/charmeleon-2.gif",
+    type1:"feu",
+    type2:"null",
+    description:"Florizarre est un imposant quadrupède à la peau verte ayant l'aspect d'un batracien, il est beaucoup plus grand et plus lourd que Bulbizarre et Herbizarre. Il porte une grande fleur sur son dos et quatre grandes feuilles, l'intérieur de ses oreilles s'est coloré de rouge et sa peau au niveau de ses pattes est désormais recouverte d'excroissances faisant penser aux verrues d'un crapaud."
+  },
+  {
+    numero:"006",
+    nom:"Dracaufeu",
+    img:"http://pokeunlock.com/wp-content/uploads/2015/01/charizard.gif",
+    shiny:"http://pokeunlock.com/wp-content/uploads/2015/01/charizard-2.gif",
+    type1:"feu",
+    type2:"null",
+    description:"Florizarre est un imposant quadrupède à la peau verte ayant l'aspect d'un batracien, il est beaucoup plus grand et plus lourd que Bulbizarre et Herbizarre. Il porte une grande fleur sur son dos et quatre grandes feuilles, l'intérieur de ses oreilles s'est coloré de rouge et sa peau au niveau de ses pattes est désormais recouverte d'excroissances faisant penser aux verrues d'un crapaud."
+  },
+  {
+    numero:"007",
+    nom:"Carapuce",
+    img:"http://pokeunlock.com/wp-content/uploads/2015/03/squirtle.gif",
+    shiny:"http://pokeunlock.com/wp-content/uploads/2015/03/squirtle-2.gif",
+    type1:"eau",
+    type2:"null",
+    description:"Florizarre est un imposant quadrupède à la peau verte ayant l'aspect d'un batracien, il est beaucoup plus grand et plus lourd que Bulbizarre et Herbizarre. Il porte une grande fleur sur son dos et quatre grandes feuilles, l'intérieur de ses oreilles s'est coloré de rouge et sa peau au niveau de ses pattes est désormais recouverte d'excroissances faisant penser aux verrues d'un crapaud."
+  },
+  {
+    numero:"008",
+    nom:"Carabaffe",
+    img:"http://pokeunlock.com/wp-content/uploads/2015/03/wartortle.gif",
+    shiny:"http://pokeunlock.com/wp-content/uploads/2015/03/wartortle.gif",
+    type1:"eau",
+    type2:"null",
+    description:"Florizarre est un imposant quadrupède à la peau verte ayant l'aspect d'un batracien, il est beaucoup plus grand et plus lourd que Bulbizarre et Herbizarre. Il porte une grande fleur sur son dos et quatre grandes feuilles, l'intérieur de ses oreilles s'est coloré de rouge et sa peau au niveau de ses pattes est désormais recouverte d'excroissances faisant penser aux verrues d'un crapaud."
+  },
+  {
+    numero:"009",
+    nom:"Tortank",
+    img:"http://pokeunlock.com/wp-content/uploads/2015/03/blastoise.gif",
+    shiny:"http://pokeunlock.com/wp-content/uploads/2015/03/blastoise-2.gif",
+    type1:"eau",
+    type2:"null",
+    description:"Florizarre est un imposant quadrupède à la peau verte ayant l'aspect d'un batracien, il est beaucoup plus grand et plus lourd que Bulbizarre et Herbizarre. Il porte une grande fleur sur son dos et quatre grandes feuilles, l'intérieur de ses oreilles s'est coloré de rouge et sa peau au niveau de ses pattes est désormais recouverte d'excroissances faisant penser aux verrues d'un crapaud."
+  }
+]
---
+++ script2.js
@@ -0,0 +1,54 @@
+// recuperer les boutons fleches sur la page html :
+var fLeft = document.querySelector("#f-left");
+var fRight = document.querySelector("#f-right");
+// initialisation de i
+var i = 0;
+// recupt emplacement des elements du pokedex
+var emplacementNumero = document.querySelector(".pkmn-number");
+var emplacementName = document.querySelector(".pkmn-name .name");
+var emplacementImgEtShiny = document.querySelector("#ecran-img");
+var emplacementTypes1et2 = document.querySelector("#types");
+var emplacementDescription = document.querySelector(".description");
+// creation des elements HTML pour insérer les infos
+var pkmnNumero = document.createElement("span");
+var pkmnName = document.createElement('span');
+var pkmnImg = document.createElement('img');
+var pkmnShiny = document.createElement('img');
+var pkmnType1 = document.createElement("span");
+var pkmnType2 = document.createElement("span");
+var pkmnDescription = document.createElement('span');
+// fonction de recupération des infos du tableau + ajout des balises crées sur le index.html
+function recuperationDonnees(i){
+pkmnNumero.textContent = pkmnDonnees[i].numero;
+  emplacementNumero.appendChild(pkmnNumero);
+pkmnName.textContent = pkmnDonnees[i].nom;
+  emplacementName.appendChild(pkmnName);
+pkmnImg.setAttribute('src', pkmnDonnees[i].img);
+pkmnShiny.setAttribute('src', pkmnDonnees[i].shiny);
+  emplacementImgEtShiny.appendChild(pkmnImg);
+  emplacementImgEtShiny.appendChild(pkmnShiny);
+    pkmnType1.classList.add(pkmnDonnees[i].type1);
+    pkmnType2.classList.add(pkmnDonnees[i].type2);
+pkmnType1.textContent = pkmnDonnees[i].type1;
+pkmnType2.textContent = pkmnDonnees[i].type2;
+  emplacementTypes1et2.appendChild(pkmnType1);
+  emplacementTypes1et2.appendChild(pkmnType2);
+pkmnDescription.textContent = pkmnDonnees[i].description;
+  emplacementDescription.appendChild(pkmnDescription);
+}
+recuperationDonnees(i);
+
+if(i > pkmnDonnees.length){
+  i = 0;
+}
+
+//fonctions au clic du bouton GAUCHE
+fLeft.addEventListener("click", function(){
+  i = i - 1;
+  recuperationDonnees(i);
+});
+
+fRight.addEventListener("click", function(){
+  i = i + 1;
+  recuperationDonnees(i);
+});
